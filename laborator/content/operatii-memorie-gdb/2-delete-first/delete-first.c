d315891f71f1c4266dfb2ccb15cb596cf9d25db3
TeofilSturzoiu <teofil.sturzoiu@gmail.com>
2022-03-18 10:07:57 +0200

    ex 2
    Signed-off-by: TeofilSturzoiu <teofil.sturzoiu@gmail.com>

--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -2,17 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern)
+{
+	//char *copy = strstr(s, pattern);
+	char *copy = malloc(100);
+	//strcpy(copy, strstr(s, pattern));
+	//strcpy(copy, copy + strlen(pattern));
+	//strcpy(s + (strstr(s, pattern) - s), copy);
+	//s + (strstr(s, pattern) - s) = copy;
+
+	strcpy(copy, s);
+	//printf("%s\n\n\n", copy);
+	strcpy(strstr(copy, pattern), strstr(copy, pattern) + strlen(pattern));
+	s = copy;
+	return s;
+}
 
 int main(){
 	char *s = "Ana are mere";
 	char *pattern = "re";
-
-	(void) s;
-	(void) pattern;
+	char *p;
 
 	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
-
+	p = delete_first(s, pattern);
+	printf("%s\n", p);
+	free(p);
 	return 0;
 }
