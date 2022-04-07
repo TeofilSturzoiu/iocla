9438a5000039fd276e88a23a20aed356b6681684
TeofilSturzoiu <teofil.sturzoiu@gmail.com>
2022-04-08 02:53:47 +0300

    lab 5 - ex 6 bonus fibonacci
    Signed-off-by: TeofilSturzoiu <teofil.sturzoiu@gmail.com>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -8,5 +8,17 @@ main:
     mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
 
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
+    mov ebx, 1
+    mov ecx, 1
+    fibo:
+        mov edx, ebx
+        mov ebx, ecx
+        add ecx, edx
+        sub eax, 1
+    loop:
+        cmp eax, 1
+        jne fibo
+
+    PRINTF32 `%d\n\x0`, ebx ; afiseaza minimul
 
     ret
